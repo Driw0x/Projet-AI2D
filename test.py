cecb3923b73a3bbdb1a1f387bc43d572159b07a4
Victor YE <victor.ye@etu.sorbonne-universite.fr>
2026-02-17 14:09:06 +0100

    Affichage des caracteristiques des donnees

--- test.py
+++ test.py
@@ -1,12 +1,18 @@
 import utils
 from pandasgui import show
+import numpy as np
 
-data = utils.read_data("data/2022-08-16_to_2023-08-15-trajectories-slice-filtered-schools.json")
+data = utils.read_data("data/2025.json")
 df = utils.AlgoPython_data(data)
 
 # show(df)
 
-print(utils.code_to_ast(df["code"][6]))
-print(utils.ast_dump(utils.code_to_ast(df["code"][6])))
+# print(utils.code_to_ast(df["code"][6]))
+# print(utils.ast_dump(utils.code_to_ast(df["code"][6])))
+
+# # Caracteristique des donnees
+# print("Nb de donnees: ", df.shape[0])
+# print("Nb d'etablissement: ", len(np.unique(df["nom_etab"])))
+# print("Nb d'utilisateur: ", len(np.unique(df["display_name"])))
 
 
